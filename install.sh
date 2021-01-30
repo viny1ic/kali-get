0eb094b0dd58f1f95aaff6726c2d985a45b3c61e
pp <pp@pp.com>
2021-01-30 20:31:18 +0530

    added uninstall.sh, made script more verbose

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
-echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
+echo "[i] Adding kali repository to apt sources"
+sudo touch /etc/apt/sources.list.d/kali.list
+sudo chmod 666 /etc/apt/sources.list.d/kali.list
+sudo echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list
+sudo chmod 644 /etc/apt/sources.list.d/kali.list
 sudo apt install gnupg
 wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
 rm archive-key.asc
-touch /etc/apt/preferences.d/kali.pref
+echo "[i] setting low priority for kali repository"
+sudo touch /etc/apt/preferences.d/kali.pref
 sudo chmod 666 /etc/apt/preferences.d/kali.pref 
 echo 'Package: *'>/etc/apt/preferences.d/kali.pref
 echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref
 echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref
 sudo chmod 644 /etc/apt/preferences.d/kali.pref
+echo "[i] Install completed. Use kali-list.sh to install kali packages"
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+echo "[i] Removing kali repository for apt sources"
+sudo rm /etc/apt/sources.list.d/kali.list
+sudo rm /etc/apt/preferences.d/kali.pref
