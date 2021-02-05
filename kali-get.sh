a6619488efd17b2270b9e3d5e1f566ad9d254e99
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-02-05 17:11:33 +0530

    export

--- kali-get.sh
+++ kali-get.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "[i] Please use me only if package is not available in ubuntu repositories."
+echo "[i] To update the package manager, please run: git pull"
 echo "[i] Check this using apt-cache search PackageName"
 echo Enter apt command:
 read input
